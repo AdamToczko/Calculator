083c801a13ea8467f27fb34e40265d0c4e62dab6
Adam Toczko <adamtoczko80@gmail.com>
2020-01-02 17:40:20 +0100

    refactoring code step 1

--- calculator.js
+++ calculator.js
@@ -3,19 +3,16 @@ const keys = calculator.querySelector('.calculator__keys')
 const display = document.querySelector('.calculator__display')
 
 const calculate = (n1, operator, n2) => {
-  let result = ''
   
   if (operator === 'add') {
-      result = parseFloat(n1) + parseFloat(n2)
+      return parseFloat(n1) + parseFloat(n2)
     } else if (operator === 'subtract') {
-      result = parseFloat(n1) - parseFloat(n2)
+      return parseFloat(n1) - parseFloat(n2)
     } else if (operator === 'multiply') {
-      result = parseFloat(n1) * parseFloat(n2)
+      return parseFloat(n1) * parseFloat(n2)
     } else if (operator === 'divide') {
-      result = parseFloat(n1) / parseFloat(n2)
+      return parseFloat(n1) / parseFloat(n2)
     }
-
-  return result
 }
 
 keys.addEventListener('click', event => {
